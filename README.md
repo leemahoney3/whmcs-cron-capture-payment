91d0da73d565a5b7ef9c5ebf944e89e2ad12365a
Lee Mahoney <lee@leemahoney.dev>
2022-09-06 21:58:19 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# whmcs-cron-capture-payment
- 
+# WHMCS Cron Capture Payment Hook
+
+A hook that runs on the cron to allow multiple payment capture attempts per day.
+
+The follow variables can be configured:
+
+```$daysBeforeDueDateToCapture``` - How many days before the due date you wish to capture payment
+e.g. if set to 3 and todays date is 6th Sept, only unpaid invoice due on the 9th Sept will be checked
+
+```$captureTimes``` - What hours of the day you wish to capture payment (e.g. '06' is 6am, '18' is 6pm)
+
+```$allowedPaymentMethods``` - If you only want to capture payments on certain payment methods, add them here (e.g. 'stripe', 'paypalcheckout')
+
+```$logErrors``` - If enabled and the capture fails, log the error in the clients log
+
+
+## How to install
+
+1. Copy the ```includes``` folder to your root WHMCS directory.
+
+## Contributions
+
+Feel free to fork the repo, make changes, then create a pull request! For ideas on what you can help with, check the project issues.
---
+++ includes/hooks/cronCapturePayment.php
@@ -0,0 +1,74 @@
+<?php
+
+use WHMCS\Carbon;
+use WHMCS\Database\Capsule;
+
+/**
+ * WHMCS Cron Capture Payment
+ *
+ * A hook that runs on the cron to allow multiple payment capture attempts per day
+ *
+ * @package    WHMCS
+ * @author     Lee Mahoney <lee@leemahoney.dev>
+ * @copyright  Copyright (c) Lee Mahoney 2022
+ * @license    MIT License
+ * @version    1.0.0
+ * @link       https://leemahoney.dev
+ */
+
+function cron_capture_payment($vars) {
+
+    # How many days before the due date you wish to capture payment
+    # e.g. if set to 3 and todays date is 6th Sept, only unpaid invoice due on the 9th Sept will be checked
+    $daysBeforeDueDateToCapture = 0;
+
+    # What hours of the day you wish to capture payment (e.g. '06' is 6am, '18' is 6pm)
+    $captureTimes = ['00', '06', '12', '18'];
+
+    # If you only want to capture payments on certain payment methods, add them here (e.g. 'stripe', 'paypalcheckout')
+    $allowedPaymentMethods = [];
+
+    # If enabled and the capture fails, log the error in the clients log
+    $logErrors = true;
+
+    /* ------------------------------------------ */
+    /* ONLY EDIT VARIABLES ABOVE THIS LINE        */
+    /* ------------------------------------------ */
+
+    # Grab the current time (only the hour needed)
+    $currentTime = Carbon::now()->format('H');
+
+    # Check if the current hour is in the $captureTimes array
+    if (in_array($currentTime, $captureTimes)) {
+     
+        # Calculate the due date based on the $daysBeforeDueDateToCapture variable
+        $theDueDate = Carbon::now()->addDays($daysBeforeDueDateToCapture)->format('Y-m-d');
+
+        # Grab all unpaid invoices that match the due date
+        $invoices = Capsule::table('tblinvoices')->where('duedate', $theDueDate)->where('status', 'Unpaid')->get();
+
+        # Loop through the invoices
+        foreach ($invoices as $invoice) {
+            
+            # If the $allowedPaymentMethods is not empty, check that the invoice's payment method is in it
+            if (!empty($allowedPaymentMethods) && !in_array($invoice->paymentmethod, $allowedPaymentMethods)) {
+                return;
+            }
+
+            # Attempt to capture payment
+            $result = localAPI('CapturePayment', [
+                'invoiceid' => $invoice->id
+            ]);
+
+            # If $logErrors is true and an error is present, log it to the clients log
+            if ($result['result'] === 'error' && $logErrors) {
+                logActivity("Automatic payment capture hook failed on invoice #{$invoice->id}: {$result['message']}", $invoice->userid);
+            }
+
+        }
+
+    }
+
+}
+
+add_hook('AfterCronJob', 1, 'cron_capture_payment');

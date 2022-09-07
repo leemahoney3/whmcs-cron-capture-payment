fbecac821834d1897803d9d8c12db262b80ec928
Lee Mahoney <lee@leemahoney.dev>
2022-09-07 21:42:21 +0100

    Slight update
    Added in check for minute per hour to run the code (to prevent multiple capture attempts per hour). Updated README with new variable.

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ The follow variables can be configured:
 ```$daysBeforeDueDateToCapture``` - How many days before the due date you wish to capture payment
 e.g. if set to 3 and todays date is 6th Sept, only unpaid invoice due on the 9th Sept will be checked
 
-```$captureTimes``` - What hours of the day you wish to capture payment (e.g. '06' is 6am, '18' is 6pm)
+```$captureHours``` - What hours of the day you wish to capture payment (e.g. '06' is 6am, '18' is 6pm)
+
+```$captureMinute``` - Which minute after the hour you wish this check to run at (e.g. if your cron is set to run every 5 minutes, can set this to 5, 10, 15, 20, 25, etc...)
 
 ```$allowedPaymentMethods``` - If you only want to capture payments on certain payment methods, add them here (e.g. 'stripe', 'paypalcheckout')
 
--- includes/hooks/cronCapturePayment.php
+++ includes/hooks/cronCapturePayment.php
@@ -12,7 +12,7 @@ use WHMCS\Database\Capsule;
  * @author     Lee Mahoney <lee@leemahoney.dev>
  * @copyright  Copyright (c) Lee Mahoney 2022
  * @license    MIT License
- * @version    1.0.0
+ * @version    1.0.1
  * @link       https://leemahoney.dev
  */
 
@@ -27,7 +27,10 @@ function cron_capture_payment($vars) {
     $daysBeforeDueDateToCapture = 0;
 
     # What hours of the day you wish to capture payment (e.g. '06' is 6am, '18' is 6pm)
-    $captureTimes = ['00', '06', '12', '18'];
+    $captureHours = ['00', '06', '12', '18'];
+
+    # Which minute after the hour you wish this check to run at (e.g. if your cron is set to run every 5 minutes, can set this to 5, 10, 15, 20, 25, etc...)
+    $captureMinute = 10;
 
     # If you only want to capture payments on certain payment methods, add them here (e.g. 'stripe', 'paypalcheckout')
     $allowedPaymentMethods = [];
@@ -39,12 +42,14 @@ function cron_capture_payment($vars) {
     /* ONLY EDIT VARIABLES ABOVE THIS LINE        */
     /* ------------------------------------------ */
 
-    # Grab the current time (only the hour needed)
-    $currentTime = Carbon::now()->format('H');
+    # Grab the current time and date
+    $currentTime    = Carbon::now()->format('H');
+    $currentMinute  = Carbon::now()->format('i');
+    $currentDay     = Carbon::now()->format('Y/m/d');
 
-    # Check if the current hour is in the $captureTimes array
-    if (in_array($currentTime, $captureTimes)) {
-     
+    # Check if the current hour is in the $captureHours array
+    if (in_array($currentTime, $captureHours) && $currentMinute == $captureMinute) {
+
         # Calculate the due date based on the $daysBeforeDueDateToCapture variable
         $theDueDate = Carbon::now()->addDays($daysBeforeDueDateToCapture)->format('Y-m-d');
 
@@ -53,7 +58,7 @@ function cron_capture_payment($vars) {
 
         # Loop through the invoices
         foreach ($invoices as $invoice) {
-            
+
             # If the $allowedPaymentMethods is not empty, check that the invoice's payment method is in it
             if (!empty($allowedPaymentMethods) && !in_array($invoice->paymentmethod, $allowedPaymentMethods)) {
                 return;
@@ -64,6 +69,7 @@ function cron_capture_payment($vars) {
                 'invoiceid' => $invoice->id
             ]);
 
+            print_r($result);
             # If $logErrors is true and an error is present, log it to the clients log
             if ($result['result'] === 'error' && $logErrors) {
                 logActivity("Automatic payment capture hook failed on invoice #{$invoice->id}: {$result['message']}", $invoice->userid);
